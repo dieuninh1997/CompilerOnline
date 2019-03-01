d54aa52d0a77b6d4320fa81e91a023a1cc7f8024
Sotatek-NinhTran <ninh.tran@sotatek.com>
2019-03-01 09:54:07 +0700

    config product

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const app = require('./src')
-// const port = process.env.PORT || 3000
-// app.listen(port, () => {
-//   console.log(`App is running on port ${port} - ${process.env.PORT}`)
-// })
-const path = require('path')
-const fs = require('fs')
-var https = require('https')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`App is running on port ${port} - ${process.env.PORT}`)
+})
+// const path = require('path')
+// const fs = require('fs')
+// var https = require('https')
 
-const certOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, './server.key')),
-  cert: fs.readFileSync(path.resolve(__dirname, './server.crt'))
-}
+// const certOptions = {
+//   key: fs.readFileSync(path.resolve(__dirname, './server.key')),
+//   cert: fs.readFileSync(path.resolve(__dirname, './server.crt'))
+// }
 
-https.createServer(certOptions, app).listen(443)
+// https.createServer(certOptions, app).listen(443)
