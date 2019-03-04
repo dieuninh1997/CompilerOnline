7bb121289d6284f131253216e96b7f9adbae6c91
Sotatek-NinhTran <ninh.tran@sotatek.com>
2019-03-04 18:07:10 +0700

    restyle codes, account page and add filter in codes page

--- src/public/javascripts/mycodes.js
+++ src/public/javascripts/mycodes.js
@@ -5,6 +5,60 @@
   $(myCodeReady)
   function myCodeReady () {
     var itemCode = $('#itemCode')
+    var selectAll = $('#select_all')
+    var selectNone = $('#select_none')
+    var filterBtn = $('#filter_btn')
+
+    filterBtn.on('click', function () {
+      var filterboxes = document.getElementsByName('filter_item')
+      var filterRadioes = document.getElementsByName('filter_time')
+      var whereLanguageFilter = []
+      var whereTimeFilter = ''
+      filterboxes.forEach(element => {
+        if (element.checked) {
+          whereLanguageFilter.push(element.value)
+        }
+      })
+      filterRadioes.forEach(radio => {
+        if (radio.checked) {
+          whereTimeFilter += radio.value
+        }
+      })
+      console.log('========================================')
+      console.log('whereLanguageFilter', whereLanguageFilter)
+      console.log('whereTimeFilter', whereTimeFilter)
+      console.log('========================================')
+      var config = {
+        whereLanguageFilter,
+        whereTimeFilter
+      }
+      $.ajax({
+        type: 'GET',
+        url: '/codes',
+        data: JSON.stringify(config),
+        dataType: 'json',
+        contentType: 'application/json'
+      }).done(function (respone) {
+        if (respone.success) {
+
+        } else {
+        }
+      })
+    })
+
+    selectAll.on('click', function () {
+      var checkboxes = document.getElementsByName('check_item')
+      checkboxes.forEach(element => {
+        element.checked = true
+      })
+    })
+
+    selectNone.on('click', function () {
+      var checkboxes = document.getElementsByName('check_item')
+      checkboxes.forEach(element => {
+        element.checked = false
+      })
+    })
 
     itemCode.on('click', function () {
       var sourceId = $('#source_id').text()
--- src/routes/codes/index.js
+++ src/routes/codes/index.js
@@ -4,11 +4,35 @@ const knex = require('../../knex')
 
 myRecentRouter.get('/', async (req, res, next) => {
   const userInfo = req.user
-  const data = await knex('compile').select().where('user_id', userInfo.id)
+
+  var data = []
+  if (userInfo) {
+    data = await knex('compile').select().where('user_id', userInfo.id)
+  } else {
+    const { whereLanguageFilter, whereTimeFilter } = req.data
+    console.log('========================================')
+    console.log('req.data', req.data)
+    console.log('========================================')
+    if (whereTimeFilter !== 'all') {
+      const timeFilter = Date.now() + parseInt(whereTimeFilter)
+      data = await knex('compile').select().whereIn('language', whereLanguageFilter).andWhere(function () {
+        this.where('created_at', '<=', timeFilter)
+      })
+      console.log('========================================')
+      console.log('data', data)
+      console.log('========================================')
+    } else {
+      data = await knex('compile').select().whereIn('language', whereLanguageFilter)
+      console.log('========================================')
+      console.log('data', data)
+      console.log('========================================')
+    }
+  }
 
   res.render('mycodes/mycodes.html', {
     webTitle: 'My codes',
     data: data
   })
 })
+
 module.exports = myRecentRouter
